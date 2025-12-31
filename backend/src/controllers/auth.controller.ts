d04e1bb9410847612e48e0b52f5230d6d9121a6b
garima shrestha <garimashrestha20@gmail.com>
2025-12-31 13:43:58 +0545

    Error solving

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -24,4 +24,25 @@ export class AuthController {
             );
         }
     }
+
+    
+    // For login
+    async login(req: Request, res: Response) {
+        try {
+            const parsedData = LoginUserDto.safeParse(req.body);
+            if(!parsedData.success) {
+                return res.status(400).json(
+                    { success: false, message: z.prettifyError(parsedData.error) }
+                );
+            }
+            const { token, existingUser } = await userService.loginUser(parsedData.data);
+            return res.status(200).json(
+                { success: true, data: existingUser, token, message: "Login success" }
+            );
+        } catch (error: Error | any ) {
+            return res.status(error.statusCode || 500).json(
+                { success: false, message: error.message || "Internal Server Error"}
+            );
+        }
+    }
 } 
